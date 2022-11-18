05336c60f915ccf7f94e0f83427fb93ddf161063
Hsing-Yu (David) Chen <davidhsingyuchen@gmail.com>
2022-11-18 10:52:59 -0800

    chore: add an older gomega version to trigger dependabot

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/onsi/gomega"
 	"github.com/spf13/afero"
 )
 
 func main() {
 	err := afero.ErrFileClosed
 	fmt.Println(err)
+	var a gomega.Assertion
+	fmt.Println(a)
 }
