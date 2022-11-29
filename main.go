847419a08f1eebfe595c7b7ec0cd35ea37a5264d
Hsing-Yu (David) Chen <davidhsingyuchen@gmail.com>
2022-11-28 18:07:47 -0800

    Modifying non-docs should trigger ci.yml

--- main.go
+++ main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/onsi/gomega"
 	"github.com/spf13/afero"
 )
 
 func main() {
 	err := afero.ErrFileClosed
 	fmt.Println(err)
-	var a gomega.Assertion
-	fmt.Println(a)
 }
